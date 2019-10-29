4ef31f604f2bebabe1b03fce88e2a03d079ac22c
nano <ccontr94@gmail.com>
2019-10-29 14:41:14 -0500

    testing out other screen

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -1,5 +1,21 @@
 <template>
-
+    <div class="conversation-box">
+        <div class="conversation1">
+            <div class="bubble1">
+                How was your day
+            </div>
+        </div>
+        <div class="conversation2">
+            <div class="bubble2">
+                yo what is going on
+            </div>
+        </div>
+        <div class="conversation1">
+            <div class="bubble1">
+                sup my name is big boi
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +25,37 @@
 </script>
 
 <style scoped>
+    .conversation1 {
+        float: left;
+        position: relative;
+        margin-top: 10%;
+        width: 100%;
+    }
+    .conversation2 {
+        float: right;
+        position: relative;
+        margin-top: 10%;
+    }
 
+    .conversation-box {
+        margin: auto;
+        width: 100%;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+
+    .bubble1 {
+        color: white;
+        display: inline-block;
+        background-color: #46ab68;
+        border-radius: 25px;
+        padding: 10px;
+    }
+
+    .bubble2 {
+        display: inline-block;
+        background-color: #e8dddc;
+        border-radius: 25px;
+        padding: 10px;
+    }
 </style>
